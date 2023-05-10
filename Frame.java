a849fb6db589eb0a3d99bbf1d2d00558086a0ef1
jakpref <107634301+jakpref@users.noreply.github.com>
2023-05-10 23:31:24 +0200

    Added text file save

---
+++ Frame.java
@@ -0,0 +1,170 @@
+package client;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class Frame extends JFrame {
+    boolean noJComboBoxListener;
+    JToggleButton encryptButton, runButton;
+    JComboBox files;
+    JTextField address;
+    JPanel topPanel;
+    PTTP_Client pttp_client;
+    public String[] ls;
+    public Frame() {
+        noJComboBoxListener = false;
+        ls = new String[]{".."};
+        this.setLocationRelativeTo(null);
+        pttp_client = new PTTP_Client();
+        this.setSize(400, 140);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        encryptButton = new JToggleButton("Encryption");
+        this.add(encryptButton, BorderLayout.SOUTH);
+        encryptButton.setVisible(true);
+
+        runButton = new JToggleButton("Run");
+        files = new JComboBox<String>(ls);
+        this.add(files, BorderLayout.CENTER);
+        files.setVisible(true);
+
+        address = new JTextField();
+
+        address.setEditable(true);
+        topPanel = new JPanel();
+        topPanel.setLayout(new GridLayout(1,2));
+        this.add(topPanel, BorderLayout.NORTH);
+        topPanel.add(address);
+        topPanel.add(runButton);
+        topPanel.setVisible(true);
+        
+        address.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!(address.getText().split("/")[0].equals("pttp:")) && !(address.getText().split("/")[0].equals("pttpu:")) )
+                {
+
+                    if (Frame.this.encryptButton.getModel().isSelected()) {
+                        address.setText("pttpu://" + address.getText());
+                    }
+                    else {
+                        address.setText("pttp://" + address.getText());
+                    }
+
+                }
+                if( address.getText().charAt(address.getText().length()-1)!='/')
+                    address.setText(address.getText() + "/");
+            }
+        });
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+            	try {
+                    String command = address.getText();
+                    String[] list = Frame.this.pttp_client.communicate(command);
+                    
+                    if(Frame.this.address.getText().contains(".")) {
+                    	File fileName = null;
+
+                    	JFileChooser saveChooser = new JFileChooser();
+    					saveChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
+    					int choice = saveChooser.showSaveDialog(Frame.this);
+    					if (choice == JFileChooser.APPROVE_OPTION) {
+    						fileName = saveChooser.getSelectedFile();
+
+    						try {
+    							OutputStreamWriter output = new  OutputStreamWriter(new FileOutputStream(fileName + ".txt"), Charset.forName("UTF-8").newEncoder());  
+    							String str = String.join("\n", list);
+    							output.write(str);
+    				            output.close();
+
+    						}catch (IOException d) {
+    							d.printStackTrace();
+    						}
+    					}
+                    }
+                    else {
+                    	Frame.this.readls(list);
+                    }
+                } catch (IOException e2) {
+                    System.out.println("IO exception");
+                }
+                Frame.this.runButton.setSelected(false);
+
+            }
+        });
+        files.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(Frame.this.files.getSelectedItem()!=null && !Frame.this.noJComboBoxListener){
+                    String[] paths = Frame.this.address.getText().split("/");
+                    String element = Frame.this.files.getSelectedItem().toString();
+                    if(element == ".." && paths.length >3) {
+                        String path = "";
+                        for(int i = 0; i< paths.length-1;i++)
+                            path += (paths[i] + "/");
+                        Frame.this.address.setText(path);
+                    }
+                    else if(element != "..") {
+                        Frame.this.address.setText(Frame.this.address.getText() + element + "/");
+                    }
+                }   
+            }
+        });
+        encryptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(Frame.this.encryptButton.isSelected() && Frame.this.address.getText().split("/")[0].equals("pttp:"))
+                {
+                    String[] path = Frame.this.address.getText().split("/");
+                    path[0] = "pttpu:";
+                    String res = "";
+                    for (String i : path)
+                        res += (i + "/");
+                    Frame.this.address.setText(res);
+                }
+                if(!Frame.this.encryptButton.isSelected() && Frame.this.address.getText().split("/")[0].equals("pttpu:"))
+                {
+                    String[] path = Frame.this.address.getText().split("/");
+                    path[0] = "pttp:";
+                    String res = "";
+                    for (String i : path)
+                        res += (i + "/");
+                    Frame.this.address.setText(res);
+                }
+            }
+        });
+        this.setVisible(true);
+    }
+    public void readls(String[] list)
+    {
+        noJComboBoxListener = true;
+        this.ls = new String[list.length+1];
+        this.ls[list.length] = "..";
+        for (int i = 0; i < list.length; i++)
+        {
+            this.ls[i] = list[i];
+        }
+        DefaultComboBoxModel model = (DefaultComboBoxModel) this.files.getModel();
+        model.removeAllElements();
+        for (String item : this.ls) {
+            model.addElement(item);
+        }
+        files.setModel(model);
+        this.files.repaint();
+        noJComboBoxListener = false;
+    }
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+    }
+}
---
+++ PTTP_Client.java
@@ -0,0 +1,105 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.net.http.WebSocketHandshakeException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+public class PTTP_Client {
+	String decodedString;
+	public PTTP_Client() {
+
+	}
+    public String[] communicate(String command) throws IOException {
+		decodedString = "";
+		String message;
+		int port;
+		String host;
+		String pathToFile = "";
+
+		message = command;
+		String[] split = message.split("/");
+
+		if (split[0].equals("pttp:")) {
+
+			port = 42750;
+			host = split[2];
+			int i=3;
+			while(i < split.length) {
+				pathToFile += ("/" + split[i]);
+				i++;
+			}
+		}
+		else if (split[0].equals("pttpu:")) {
+
+			port = 42751;
+			host = split[2];
+			int i=3;
+			while(i < split.length) {
+				pathToFile += ("/" + split[i]);
+				i++;
+			}
+		}
+		else {
+			System.out.println("Incorrect input.");
+			return decodedString.split(System.lineSeparator());
+		}
+		//pttp://localhost/test
+		try (Socket socket = new Socket(host, port)) {
+			socket.setSoTimeout(1000);
+
+			OutputStream output = socket.getOutputStream();
+			PrintWriter writer = new PrintWriter(output, true);
+
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			if(port == 42750) {
+
+				writer.println("GET " + pathToFile + " PTTP/1.0");
+				String line;
+				while ((line = reader.readLine()) != null) {
+					decodedString += (line+ "\n");
+					System.out.println(line);
+				}
+				socket.close();
+			}
+			else if(port == 42751) {
+
+				String encodedString = Base64.getEncoder().encodeToString(("GET " + pathToFile + " PTTP/1.0").getBytes());
+				writer.println(encodedString);
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					byte[] decodedBytes = Base64.getDecoder().decode(line);
+					decodedString = new String(decodedBytes);
+					System.out.println(decodedString);
+				}
+				socket.close();
+			}
+		}catch (SocketTimeoutException ste){
+
+		}catch (UnknownHostException uhe) {
+			System.out.println("Unknown Host\n");
+		}catch (ConnectException ce){
+			System.out.println("Failed to Connect\n");
+		}
+		String[] out = decodedString.split("\n");
+		return out;
+	}
+}
